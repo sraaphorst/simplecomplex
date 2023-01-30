91e009dceb7e268c464fdef07e1edfd0dc3d5932
Sebastian Raaphorst <sraaphorst@gmail.com>
2023-01-29 22:44:46 -1000

    Added and organized test cases and fixed bugs.

--- src/test/kotlin/math/ComplexTest.kt
+++ src/test/kotlin/math/ComplexTest.kt
@@ -5,66 +5,179 @@ package math
 import io.kotest.core.spec.style.StringSpec
 import io.kotest.property.forAll
 import io.kotest.property.Arb
-import io.kotest.property.arbitrary.arbitrary
-import io.kotest.property.arbitrary.int
-import io.kotest.property.arbitrary.numericDouble
-import io.kotest.property.arbitrary.pair
+import io.kotest.property.arbitrary.*
 import kotlin.math.PI
 
+val doubleArb = Arb.numericDouble(-1e3, 1e3)
+val angleArb = Arb.numericDouble(0.0, 2.0 * PI)
+
 val cartesianArb = arbitrary {
-    val re = Arb.numericDouble(-1e3, 1e3).bind().toDouble()
-    val im = Arb.numericDouble(-1e3, 1e3).bind().toDouble()
+    val re = doubleArb.bind().toDouble()
+    val im = doubleArb.bind().toDouble()
     Cartesian(re, im)
 }
 
 val polarArb = arbitrary {
-    val r = Arb.numericDouble(-1e3, 1e3).bind().toDouble()
-    val theta = Arb.numericDouble(0.0, 2 * PI).bind().toDouble()
+    val r = doubleArb.bind().toDouble()
+    val theta = angleArb.bind().toDouble()
     Polar(r, theta)
 }
 
 class ComplexTest: StringSpec({
-    "Using I suffix on a number creates an imaginary number" {
-        forAll(Arb.int(-1000, 1000)) { n ->
-            n.I == Cartesian(0, n)
+    "Cartesian: using I suffix on a number creates an imaginary element" {
+        forAll(doubleArb) { n ->
+            Compare.almostEquals(n.I, Cartesian(0, n))
         }
     }
 
-    "Inversion of cartesian times itself is 1" {
+    "Polar: using J suffix on a number creates an imaginary element" {
+        forAll(doubleArb) { n ->
+            Compare.almostEquals(n.J, Polar(n, PI / 2))
+        }
+    }
+
+    "Cartesian: additive inverses exist" {
         forAll(cartesianArb) { c ->
-            // We do not want numbers extremely close to 0.
+            Compare.almostEquals(Cartesian.ZERO, c + (-c))
+        }
+    }
+
+    "Cartesian: multiplicative inverses exist" {
+        forAll(cartesianArb) { c ->
             Compare.almostEquals(Cartesian.ZERO, c) || Compare.almostEquals(Cartesian.ONE, c * (1 / c))
         }
     }
 
-    "Inversion of polar times itself is 1" {
+    "Polar: additive inverses exist" {
         forAll(polarArb) { p ->
-            // We do not want numbers extremely close to 0.
+            Compare.almostEquals(Polar.ZERO, p + (-p))
+        }
+    }
+
+    "Polar: multiplicative inverses exist" {
+        forAll(polarArb) { p ->
             Compare.almostEquals(Polar.ZERO, p) || Compare.almostEquals(Polar.ONE, p * (1 / p))
         }
     }
 
-    "Multiplying by cartesian conjugate gives real" {
+    "Cartesian: addition is commutative" {
+        forAll(Arb.pair(cartesianArb, cartesianArb)) { (c1, c2) ->
+            Compare.almostEquals(c1 + c2, c2 + c1)
+        }
+    }
+
+    "Cartesian: addition is associative" {
+        forAll(Arb.triple(cartesianArb, cartesianArb, cartesianArb)) { (c1, c2, c3) ->
+            Compare.almostEquals(c1 + (c2 + c3), (c1 + c2) + c3)
+        }
+    }
+
+    "Cartesian: subtraction is equivalent to addition with additive inverses" {
+        forAll(Arb.pair(cartesianArb, cartesianArb)) { (c1, c2) ->
+            c1 - c2 == c1 + (-c2)
+        }
+    }
+
+    "Cartesian: multiplication is commutative" {
+        forAll(Arb.pair(cartesianArb, cartesianArb)) { (c1, c2) ->
+            c1 * c2 == c2 * c1
+        }
+    }
+
+    "Cartesian: multiplication is associative" {
+        forAll(Arb.triple(cartesianArb, cartesianArb, cartesianArb)) { (c1, c2, c3) ->
+            Compare.almostEquals(c1 * (c2 * c3), (c1 * c2) * c3)
+        }
+    }
+
+    "Cartesian: multiplication by scalars is commutative" {
+        forAll(Arb.pair(doubleArb, cartesianArb)) { (n, c) ->
+            Compare.almostEquals(n * c, c * n)
+        }
+    }
+
+    "Cartesian: multiplication by scalars is associative" {
+        forAll(Arb.triple(doubleArb, doubleArb, cartesianArb)) { (n1, n2, c) ->
+            Compare.almostEquals((n1 * n2) * c, n1 * (n2 * c))
+        }
+    }
+
+    "Cartesian: division is equivalent to multiplication with multiplicative inverses" {
+        forAll(Arb.pair(cartesianArb, cartesianArb)) { (c1, c2) ->
+            Compare.almostEquals(Cartesian.ZERO, c2) ||
+                    Compare.almostEquals(c1 / c2, c1 * (1 / c2))
+        }
+    }
+
+    "Polar: addition is commutative" {
+        forAll(Arb.pair(polarArb, polarArb)) { (p1, p2) ->
+            Compare.almostEquals(p1 + p2, p2 + p1)
+        }
+    }
+
+    "Polar: addition is associative" {
+        forAll(Arb.triple(polarArb, polarArb, polarArb)) { (p1, p2, p3) ->
+            Compare.almostEquals(p1 + (p2 + p3), (p1 + p2) + p3)
+        }
+    }
+
+    "Polar: multiplication is commutative" {
+        forAll(Arb.pair(polarArb, polarArb)) { (p1, p2) ->
+            Compare.almostEquals(p1 * p2, p2 * p1)
+        }
+    }
+
+    "Polar: multiplication is associative" {
+        forAll(Arb.triple(polarArb, polarArb, polarArb)) { (p1, p2, p3) ->
+            Compare.almostEquals(p1 * (p2 * p3), (p1 * p2) * p3)
+        }
+    }
+
+    "Polar: multiplication by scalars is commutative" {
+        forAll(Arb.pair(doubleArb, polarArb)) { (n, p) ->
+            Compare.almostEquals(n * p, p * n)
+        }
+    }
+
+    "Polar: multiplication by scalars is associative" {
+        forAll(Arb.triple(doubleArb, doubleArb, polarArb)) { (n1, n2, p) ->
+            Compare.almostEquals((n1 * n2) * p, n1 * (n2 * p))
+        }
+    }
+
+    "Polar: division is equivalent to multiplication with multiplicative inverses" {
+        forAll(Arb.pair(polarArb, polarArb)) { (p1, p2) ->
+            Compare.almostEquals(Polar.ZERO, p2) ||
+                    Compare.almostEquals(p1 / p2, p1 * (1 / p2))
+        }
+    }
+
+
+    "Cartesian: multiplying by conjugate gives real" {
         forAll(cartesianArb) { c ->
-            val value = (c.conjugate * c)
-            Compare.almostEquals(0.0, value.im)
+            Compare.almostEquals(0, (c.conjugate * c).im)
         }
     }
 
-    "Multiplying by polar conjugate gives real" {
+    "Polar: multiplying by conjugate gives real" {
         forAll(polarArb) { p ->
-            val value = (p.conjugate * p)
-            Compare.almostEquals(0.0, value.theta)
+            Compare.almostEquals(0, (p.conjugate * p).theta)
         }
     }
 
-    "Conjugate is self inverting" {
+    "Cartesian: conjugate is self inverting" {
         forAll(cartesianArb) { c ->
             Compare.almostEquals(c, c.conjugate.conjugate)
         }
     }
 
-    "Multiplying by complex conjugate gives square of magnitude" {
+    "Polar: conjugate is self inverting" {
+        forAll(polarArb) { p ->
+            Compare.almostEquals(p, p.conjugate.conjugate)
+        }
+    }
+
+    "Cartesian: multiplying by conjugate gives square of magnitude" {
         forAll(cartesianArb) { c ->
             val value = (c.conjugate * c)
             Compare.almostEquals(c.magnitude * c.magnitude, value.re)
@@ -72,26 +185,34 @@ class ComplexTest: StringSpec({
         }
     }
 
-    "Multiplying two numbers in polar coordinates gives same result as multiplying in cartesian coordinates" {
+    "Polar: multiplying by conjugate gives square of magnitude" {
+        forAll(polarArb) { p ->
+            val value = (p.conjugate * p)
+            Compare.almostEquals(p.magnitude * p.magnitude, value.r)
+            Compare.almostEquals(0, value.theta)
+        }
+    }
+
+    "Multiplying two numbers in polar coordinates gives same result as in cartesian coordinates" {
         forAll(Arb.pair(polarArb, polarArb)) { (p1, p2) ->
             Compare.almostEquals((p1 * p2).toCartesian, (p1.toCartesian * p2.toCartesian))
         }
     }
 
-    "Multiplying two numbers in cartesian coordinates gives same result as multiplying in polar coordinates" {
+    "Multiplying two numbers in cartesian coordinates gives same result as in polar coordinates" {
         forAll(Arb.pair(cartesianArb, cartesianArb)) { (c1, c2) ->
             Compare.almostEquals((c1 * c2).toPolar, (c1.toPolar * c2.toPolar))
         }
     }
 
-    "Dividing two numbers in polar coordinates gives same result as dividing in cartesian coordinates" {
+    "Dividing two numbers in polar coordinates gives same result as in cartesian coordinates" {
         forAll(Arb.pair(polarArb, polarArb)) { (p1, p2) ->
             Compare.almostEquals(0.0, p2.magnitude) ||
             Compare.almostEquals((p1 / p2).toCartesian, (p1.toCartesian / p2.toCartesian))
         }
     }
 
-    "Dividing two numbers in cartesian coordinates gives same result as dividing in polar coordinates" {
+    "Dividing two numbers in cartesian coordinates gives same result as in polar coordinates" {
         forAll(Arb.pair(cartesianArb, cartesianArb)) { (c1, c2) ->
             Compare.almostEquals(0.0, c2.magnitude) ||
             Compare.almostEquals((c1 / c2).toPolar, (c1.toPolar / c2.toPolar))
